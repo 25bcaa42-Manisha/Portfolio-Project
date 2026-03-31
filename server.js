b3c14d02142ea5690011aa88a52ae48158376e49
25bcaa42-Manisha <25bcaa42@kristujayanti.com>
2026-04-01 00:53:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ db.connect(err => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
 app.post('/add-user', (req, res) => {
     const { name, email, message } = req.body;
 
